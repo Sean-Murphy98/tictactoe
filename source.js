a83c91e5fee78e0cbfad9e706dd46cb0d5a380c9
Sean-Murphy98 <92535432+Sean-Murphy98@users.noreply.github.com>
2025-10-23 23:51:16 -0700

    Win logic complete, todo display, linting, tidying, input handling for console

--- source.js
+++ source.js
@@ -1,24 +1,46 @@
 const gameboard = (function (){
-    let gameboard = ["","","","","","","","",""];
-    let length = gameboard.length;
-    let rowSize = Math.sqrt(length);
+    let gameboard = [".",".",".",".",".",".",".",".","."];
     function placeShape (position, icon){
         if (position>8 || position<0){
             alert("Square must be placed inside grid!");
         }
-        else if (gameboard[position] == ""){
+        else if (gameboard[position] == "."){
             gameboard[position] = icon;
         }
         else{
             alert("Invalid selection!");
         }
     }
+    function resetBoard () {
+        gameboard = [".",".",".",".",".",".",".",".","."];
+    }
     function printBoard () {
-        for (let i=0; i<rowSize; i++){
-            console.log(gameboard[0 + rowSize*i],gameboard[1 + rowSize*i],gameboard[2 + rowSize*i])
+        for (let i=0; i<3; i++){
+            console.log(gameboard[0 + 3*i],gameboard[1 + 3*i],gameboard[2 + 3*i])
         }
     }
-    return { gameboard, placeShape, printBoard };
+    function checkWin () {
+        for (let j=0; j<3; j++){
+            //Checks if winner on columns
+            if (gameboard[j] != "." && (gameboard[j]==gameboard[j+3] && gameboard[j]==gameboard[j+3*2])){
+                return gameboard[j];
+            }
+            //Checks if winner on rows
+            if (gameboard[0 + 3*j] != "." && (gameboard[0 + 3*j]==gameboard[1 + 3*j] && gameboard[0 + 3*j]==gameboard[2 + 3*j])){
+                return gameboard[j];
+            }
+            //Checks if winner on first diagonal
+            if (gameboard[0] != "." && (gameboard[0] == gameboard[4] && gameboard[0]==gameboard[8])){
+                return gameboard[0];
+            }
+            //Checks if winner on second diagonal
+            if (gameboard[2] != "." && (gameboard[2] == gameboard[4] && gameboard[2]==gameboard[6])){
+                return gameboard[0];
+            }
+        } 
+        return null;
+    }
+    return { gameboard, placeShape, resetBoard, printBoard, checkWin };
 })
 
 function createUser (name, icon) {
@@ -27,8 +49,10 @@ function createUser (name, icon) {
 
 const gameController = (function (){
     const board = gameboard();
-    const user1 = createUser("User1", "X");
-    const user2 = createUser("User2", "O");
+    let name1 = prompt("Please enter your name, Player 1!");
+    const user1 = createUser(name1, "X");
+    let name2 = prompt("Please enter your name, Player 2!");
+    const user2 = createUser(name2, "O");
     let turnNum = 0;
     const players = [user1, user2];
     let activePlayer = players[0];
@@ -40,8 +64,24 @@ const gameController = (function (){
         board.printBoard()
     }
     const playTurn = (position) => {
-        board.placeShape(position, activePlayer.icon)
-        console.log(`Placing ${activePlayer.name}'s Shape!`)
+        board.placeShape(position, activePlayer.icon);
+        console.log(`Placing ${activePlayer.name}'s Shape!`);
+        turnNum++;
+        if(board.checkWin()){
+            console.log(`${activePlayer.name} Wins!`);
+            board.resetBoard();
+            printNewTurn();
+            activePlayer = players[0];
+            turnNum = 0;
+            return
+        }
+        else if (turnNum==9){
+            console.log("It's a tie!");
+            board.resetBoard();
+            printNewTurn();
+            turnNum = 0;
+            return
+        }
         switchActivePlayer();
         printNewTurn();
     }
